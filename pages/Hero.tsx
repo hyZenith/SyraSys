82816db8db1d5f1331f6859c7ddd64b3396b6540
zen_.ith <124890239+hyZenith@users.noreply.github.com>
2026-01-28 11:14:50 +0530

    add pages

---
+++ pages/Hero.tsx
@@ -0,0 +1,17 @@
+import Header from '@/components/Header'
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className=''>
+      {/* <Header /> */}
+      <section className="h-screen w-full flex flex-col justify-center items-center text-white">
+
+      </section>
+
+    </div>
+
+  )
+}
+
+export default Hero
