204fd21f420ff0c4c1f6d1924b06e895e67206df
zen_.ith <124890239+hyZenith@users.noreply.github.com>
2026-02-10 00:00:45 +0530

    update app folders

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
 import Hero from "@/pages/Hero";
-import Sidebar from "@/pages/Sidebar";
-import Work from "@/pages/Work";
 export default function Home() {
   return (
-      <main className="">
-        <Sidebar />
-        <Hero />
-        <Work />
-      </main>
+    <Hero />
   );
 }
