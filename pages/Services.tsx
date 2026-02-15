07a37441af36fb5a464c45f49f97249c9dff15b0
zen_.ith <124890239+hyZenith@users.noreply.github.com>
2026-02-15 22:02:07 +0530

    add service page

---
+++ pages/Services.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ServicesSection from '@/components/ServicesSection'
+
+const Services = () => {
+  return (
+    <div className='min-h-screen bg-white'>
+      <div className=" p-3  bg-white rounded-4xl mt-10 border">
+        <ServicesSection />
+      </div>
+    </div>
+  )
+}
+
+export default Services
