666157d51b9494c60eaf90fd84ca11fe90fe79ba
zen_.ith <124890239+hyZenith@users.noreply.github.com>
2026-03-05 16:03:42 +0530

    feat: add AboutUs page

---
+++ pages/AboutUs.tsx
@@ -0,0 +1,7 @@
+export default function AboutUs() {
+    return (
+        <section className="relative px-6 lg:px-16 py-20 md:py-28 flex justify-center items-center min-h-screen overflow-hidden">
+            <div> welcome to about us</div>
+        </section>
+    )
+}
