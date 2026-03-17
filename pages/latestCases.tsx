9b880bb1de9de26bdfbeb4796707aba4b2587536
hyZenith <hey.h1fz@gmail.com>
2026-03-17 12:28:20 +0530

    add lastest Cases

---
+++ pages/latestCases.tsx
@@ -0,0 +1,9 @@
+
+export default function LatestCases() {
+    return (
+        <section>
+            <h1>Latest Cases</h1>
+            <p>This is the Latest Cases page.</p>
+        </section>
+    )
+}
